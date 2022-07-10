945904105c0c7fd05dfe98bdec2d4e26ea74e11a
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2022-07-10 17:28:43 -0500

    Update threadpool to use std thread instead of os-dep thread stuff, also properly close the threads

--- threadpool.hpp
+++ threadpool.hpp
@@ -5,102 +5,54 @@
 #include <mutex>
 #include <condition_variable>
 
-#if defined(_WIN32) || defined(_WIN64)
-  #define __THREADPOOL_USE_WINDOWS__
-  #include <Windows.h>
-#endif
-
 #include "wsq.hpp"
 
 class ThreadPool {
   struct mutex;
   struct cvar;
 
-#ifdef __THREADPOOL_USE_WINDOWS__
-  struct mutex {
-    CRITICAL_SECTION _value;
-
-    mutex() {
-      InitializeCriticalSection(&this->_value);
-    }
-
-    void lock() {
-      EnterCriticalSection(&this->_value);
-    }
-
-    void unlock() {
-      LeaveCriticalSection(&this->_value);
-    }
-  };
-
-  struct cvar {
-    CONDITION_VARIABLE _value;
-
-    cvar() {
-      InitializeConditionVariable(&this->_value);
-    }
-
-    void sleep(mutex* m) {
-      SleepConditionVariableCS(&this->_value, (CRITICAL_SECTION*)m, INFINITE);
-    }
-
-    void wake_one() {
-      WakeConditionVariable(&this->_value);
-    }
-
-    void wake_all() {
-      WakeAllConditionVariable(&this->_value);
-    }
-  };
-
-  struct thread {
-    HANDLE _value;
-
-    thread() {}
-    void create(DWORD (*thread_main)(PVOID), void* data) {
-      this->_value = CreateThread(0, 0, thread_main, data, 0, 0);
-    }
-    void terminate() {
-      TerminateThread(this->_value, 0);
-    }
-  };
-#endif
-
   // Threads
   std::int64_t _thread_count;
-  thread* _threads;
+  std::thread* _threads;
 
-  // Synchronization
-  mutex _work_start_mutex;
-  cvar _work_start_cvar;
+  std::mutex _work_start_mutex;
+  std::condition_variable _work_start_cvar;
 
-  mutex _work_done_mutex;
-  cvar _work_done_cvar;
+  std::mutex _work_done_mutex;
+  std::condition_variable _work_done_cvar;
 
+  std::atomic_int64_t _initialized_count;
   std::atomic_int64_t _working_count;
   std::atomic_int64_t _waiting_count;
   std::atomic_bool _done_spin_lock;
   std::atomic_bool _should_notify;
+  std::atomic_bool _stop_working;
 
   using work = void (*)();
   WorkStealingQueue<work> _work_queue;
 
-#ifdef __THREADPOOL_USE_WINDOWS__
-  static DWORD WINAPI thread_main(PVOID data) {
+  static void thread_main(void* data) {
     ThreadPool* pool = (ThreadPool*)data;
-    printf("thread here!\n");
+    pool->_initialized_count.fetch_add(1, std::memory_order_relaxed);
+
+    auto start_lock = std::unique_lock<std::mutex>(pool->_work_start_mutex, std::defer_lock);
 
     while(true) {
-      pool->_work_start_mutex.lock();
+      start_lock.lock();
       pool->_waiting_count.fetch_add(1, std::memory_order_relaxed); // tell main thread we are waiting
 
-      // sleep until we have work to do
-      while(pool->_work_queue.empty()) {
-        pool->_work_start_cvar.sleep(&pool->_work_start_mutex);
+      // sleep until we have work to do or we need to exit
+      while(pool->_work_queue.empty() && !pool->_stop_working.load(std::memory_order_relaxed)) {
+        pool->_work_start_cvar.wait(start_lock);
       }
 
-      pool->_work_start_mutex.unlock();
+      start_lock.unlock();
       pool->_waiting_count.fetch_sub(1, std::memory_order_relaxed); // tell main thread we are no longer waiting
+                                                                    //
+      // exit the thread
+      if(pool->_stop_working.load(std::memory_order_relaxed)) {
+        break;
+      }
 
       // grab work while there is work remaining and run it
       auto work = pool->_work_queue.steal();
@@ -124,80 +76,89 @@ class ThreadPool {
         // spin lock until we have confirmation from the main thread that
         // it knows we are done working
         while(pool->_should_notify.load(std::memory_order_relaxed) && !pool->_done_spin_lock.load(std::memory_order_relaxed)) {
-          pool->_work_done_cvar.wake_all();
+          pool->_work_done_cvar.notify_all();
         }
       }
     }
 
-    return 0;
+    pool->_initialized_count.fetch_sub(1, std::memory_order_relaxed);
   }
-#endif
-
-  static DWORD WINAPI debug(PVOID data) {
-    ThreadPool* pool = (ThreadPool*)data;
-
-    while(true) {
-      printf("_thread_count: %lld\n", pool->_thread_count);
-      printf("_working_count: %lld\n", pool->_working_count.load());
-      printf("_waiting_count: %lld\n", pool->_waiting_count.load());
-      printf("_done_spin_lock: %d\n", pool->_done_spin_lock.load() ? 1 : 0);
-      printf("_should_notify: %d\n", pool->_should_notify.load() ? 1 : 0);
-
-      Sleep(500);
-    }
-  }
-
-  static inline thread dbg_thread;
 
   // API
   public:
+  // Create a thread pool with the specified (power of two) queue_size and thread_count
   ThreadPool(int64_t queue_size = 1024, int64_t thread_count = std::thread::hardware_concurrency()) :
-    _work_queue(queue_size)
+    // init internals
+    _work_queue(queue_size),
+    _work_start_mutex(),
+    _work_start_cvar(),
+
+    _work_done_mutex(),
+    _work_done_cvar()
   {
-    this->_work_done_mutex = mutex();
-    this->_work_start_cvar = cvar();
-
-    this->_work_done_mutex = mutex();
-    this->_work_done_cvar = cvar();
-
+    // init internals
+    this->_initialized_count.store(0);
     this->_working_count.store(0);
     this->_waiting_count.store(0);
     this->_done_spin_lock.store(false);
     this->_should_notify.store(true);
+    this->_stop_working.store(false);
 
     // create threads
     this->_thread_count = thread_count;
-    this->_threads = (thread*)malloc(sizeof(thread) * this->_thread_count);
+    this->_threads = new std::thread[this->_thread_count];
     for(std::int64_t i = 0; i < this->_thread_count; i += 1) {
-      this->_threads[i].create(thread_main, this);
+      this->_threads[i] = std::thread(thread_main, this);
+      this->_threads[i].detach();
     }
-
-    //dbg_thread.create(debug, this);
 
     // spin lock until all threads are created (required for synchronization)
     while(this->_waiting_count.load() != this->_thread_count) {}
-    //  printf("here %lld!\n", this->_waiting_count.load());
-    //}
   }
 
+  // Destroy a thread pool, waiting for all threads to complete work
   ~ThreadPool() {
-    for(std::int64_t i = 0; i < this->_thread_count; i += 1) {
-      this->_threads[i].terminate();
+    this->join();
+    // wait until all threads are waiting
+    while(this->_waiting_count.load() != this->_thread_count) {}
+    this->_stop_working.store(true);
+
+    // spin lock until all threads are going to quit, and spam notify to
+    // make sure they all get the message
+    while(this->_initialized_count.load() != 0) {
+      this->_work_start_cvar.notify_all();
     }
 
-    free(this->_threads);
+    delete[] this->_threads;
   }
 
   using work_id = std::int64_t;
+  // Push work onto the threadpools queue
   work_id push(work w) {
     this->_work_queue.push(w);
     return 0;
   }
 
+  // Query if the threadpool has finished the current batch of work
   bool finished() {
     return this->_work_queue.empty() && this->_working_count.load() == 0;
   }
 
+  // Begin working but dont wait on threads to complete (defer joining to a later stage)
+  void start() {
+    this->_should_notify.store(false);
+
+    auto wq_size = this->_work_queue.size();
+    if(wq_size < this->_thread_count) {
+      for(std::int64_t i = 0; i < wq_size; i += 1) {
+        this->_work_start_cvar.notify_one();
+      }
+    } else {
+      this->_work_start_cvar.notify_all();
+    }
+  }
+
+  // Begin working and wait until all threads have finished their work
   void join() {
     this->_should_notify.store(true, std::memory_order_relaxed);
 
@@ -205,18 +166,20 @@ class ThreadPool {
     auto wq_size = this->_work_queue.size();
     if(wq_size < this->_thread_count) {
       for(std::int64_t i = 0; i < wq_size; i += 1) {
-        this->_work_start_cvar.wake_one();
+        this->_work_start_cvar.notify_one();
       }
     } else {
-      this->_work_start_cvar.wake_all();
+      this->_work_start_cvar.notify_all();
     }
 
     // sleep main thread until work is done
-    this->_work_done_mutex.lock();
+    auto done_lock = std::unique_lock<std::mutex>(this->_work_done_mutex, std::defer_lock);
+    done_lock.lock();
+
     while(!this->_work_queue.empty() && this->_working_count.load(std::memory_order_relaxed) != 0) {
-      this->_work_done_cvar.sleep(&this->_work_done_mutex);
+      this->_work_done_cvar.wait(done_lock);
     }
-    this->_work_done_mutex.unlock();
+    done_lock.unlock();
 
     // notify the notifying thread that we are awakened
     this->_done_spin_lock.store(true, std::memory_order_relaxed);
@@ -227,20 +190,4 @@ class ThreadPool {
     // reset previous spin lock state
     this->_done_spin_lock.store(false, std::memory_order_relaxed);
   }
-
-  void start() {
-    this->_should_notify.store(false);
-
-    auto wq_size = this->_work_queue.size();
-    if(wq_size < this->_thread_count) {
-      for(std::int64_t i = 0; i < wq_size; i += 1) {
-        this->_work_start_cvar.wake_one();
-      }
-    } else {
-      this->_work_start_cvar.wake_all();
-    }
-  }
-
-  // add some kind of ---thin--- way to
-  // query if some *specific* work in the queue has finished
 };
